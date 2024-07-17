3c77bdbeb9953e52562fd6d4ce4f6cb0d72e1b79
TomHarperDev <tom.harper347@gmail.com>
2024-07-17 23:10:09 +0100

    this is to add a plaindrome checker to my project

---
+++ CodingChallenges/Palindromes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class Palindromes
+    {
+        public Palindromes(string palindrome)
+        {
+            StringBuilder reversedString = new StringBuilder();
+
+            for(int i = palindrome.Length - 1; i >= 0; i--)
+            {
+                reversedString.Append(palindrome[i]);
+            }
+            bool anwser = (palindrome == Convert.ToString(reversedString)) ? true : false;
+            Console.WriteLine(anwser);
+        }
+    }
+}
