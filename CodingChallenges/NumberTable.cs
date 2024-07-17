4b34069b744f2d51852dbb484c92d8cdbf504927
ThomasHarper07 <tom.harper347@gmail.com>
2024-07-17 14:36:16 +0100

    adds in the number table challenge

---
+++ CodingChallenges/NumberTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class NumberTable
+    {
+        public NumberTable(string symbol, int number) 
+        {
+            StringBuilder row = new StringBuilder();
+            number++;
+            int incrementer  = 0;
+            for (int j = 0; j < number; j++)
+            {
+                for (int i = 0; i < number; i++)
+                {
+                    row.Append(" " + (i + incrementer));
+                }
+                Console.WriteLine(Convert.ToString(row));
+                row.Clear();
+                incrementer++;
+            }
+        }
+    }
+}
