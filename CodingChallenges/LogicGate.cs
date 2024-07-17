b95b7624147389a4f8d1f951a7818bc9f2a3fba2
TomHarperDev <tom.harper347@gmail.com>
2024-07-17 23:37:16 +0100

    this adds the logic gate question to the project

---
+++ CodingChallenges/LogicGate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class LogicGate
+    {
+        public LogicGate(string Operator, bool Input1, bool Input2) 
+        {
+            bool anwser = false;
+            switch (Operator.ToLower())
+            {
+                case "or":
+                    anwser = (Input1 || Input2) ? true : false;
+                    break;
+
+                case "and":
+                    anwser = (Input1 && Input2) ? true : false;
+                    break;
+
+                case "xor":
+                    anwser = (Input1 != Input2) ? true : false;
+                    break;
+
+                case "nand":
+                    anwser = !(Input1 && Input2) ? true : false;
+                    break;
+
+                case "nor":
+                    anwser = !(Input1 || Input2) ? true : false;
+                    break;
+
+                default:
+                    Console.WriteLine("Please enter a valid operator (or, and, xor, nand or nor)");
+                    break;
+            }
+            Console.WriteLine(anwser);
+
+            
+        }
+    }
+}
