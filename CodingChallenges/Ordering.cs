99832b9ce3b0f647b0b7267b53ad5fe27669ac78
TomHarperDev <tom.harper347@gmail.com>
2024-07-18 00:11:17 +0100

    this adds the ordering code

---
+++ CodingChallenges/Ordering.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class Ordering
+    {
+        public Ordering(int[] input, string orderBy) 
+        {
+            List<int> myList = new List<int>();
+            foreach (int i in input)
+            {
+                myList.Add(i);
+            }
+            if (orderBy.ToLower() == "ascending")
+            {
+                myList.Sort();
+            }
+            else
+            {
+                myList.Reverse();
+            }
+        }
+    }
+}
