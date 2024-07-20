422b41a0b58953afc55f3aa52b7878bc4f411f63
Tom <tom.harper347@gmail.com>
2024-07-21 00:53:53 +0100

    this adds the factorial challenge to the project

---
+++ CodingChallenges/1_FactorialFinder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class _1_FactorialFinder
+    {
+        public _1_FactorialFinder()
+        {
+            Console.WriteLine("Please enter a number to find a factorial of: ");
+            Console.WriteLine(Factorial(Convert.ToInt32(Console.ReadLine())));
+        }
+
+        public int Factorial(int num)
+        {
+            if (num == 0)
+            {
+                return 1;
+            }
+            return num * Factorial(num - 1);
+        }
+    }
+}
