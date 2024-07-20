63ecc2c51cdc0557d147dea116af998ddbdd32e4
Tom <tom.harper347@gmail.com>
2024-07-20 01:03:24 +0100

    Adds the item merge challenge into the project

---
+++ CodingChallenges/ItemMerge.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingChallenges
+{
+    internal class ItemMerge
+    {
+        public ItemMerge(List<string> WeekA, List<string> WeekB) 
+        {
+            List<string> Unique = new List<string>();
+            Unique.AddRange(WeekA);
+
+            for (int i = 0; i < WeekB.Count; i++)
+            {
+                if (Unique.Contains(WeekB[i]))
+                {
+                    Unique.Remove(WeekB[i]);
+                }
+            }
+            foreach (string item in Unique)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
