40b919b290e3d62eecc14dc2d18a09a33e70447a
TomHarperDev <tomharper347@gmail.com>
2024-07-26 22:40:53 +0100

    needs to be refactored and implemented using classes but works

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Text;
@@ -11,7 +12,51 @@ namespace CodingChallenges
     {
         static void Main(string[] args)
         {
-            _5_FruitMachine machine = new _5_FruitMachine();
+            happyNumber(19);
+            Console.ReadLine();
         }
+        public static void happyNumber(int num)
+        {
+            List<int> list = new List<int>();
+            list.Add(num % 10);
+            list.Add((num / 10) % 10);
+            num = 0;
+            foreach (int i in list)
+            {
+                num = num + i * i;
+                //Console.WriteLine(i);
+                //Console.WriteLine(i*i);
+            }
+            Console.WriteLine(num);
+            if (isHappyNumber(num) == true)
+            {
+                Console.WriteLine("Well done this is a happy number");
+            }
+            else
+            {
+                Console.WriteLine("Iterate again");
+                Console.ReadLine();
+                happyNumber(num);
+            }
+        }
+        public static bool isHappyNumber(int num)
+        {
+            List<int> digits = new List<int>();
+            digits.Add(num % 10);
+            digits.Add((num / 10) % 10);
+            digits.Add((num / 100) % 10);
+
+            if (digits.Sum() == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        
     }
+   
 }
