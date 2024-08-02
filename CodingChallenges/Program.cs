018b0cabb6d6f155baea1ca9556aa50bcb898452
TomHarperDev <tomharper347@gmail.com>
2024-08-02 20:37:08 +0100

    need to refactor the following code

--- CodingChallenges/Program.cs
+++ CodingChallenges/Program.cs
@@ -12,9 +12,55 @@ namespace CodingChallenges
     {
         static void Main(string[] args)
         {
-            happyNumber(19);
+            NumToEnglish(408);
             Console.ReadLine();
         }
+
+        public static void NumToEnglish(int num)
+        {
+            // this wokrs for 1-20 and can show hundreds, however, it does not work for tens above 20
+            // doesnt work when any digit above 0 is in tens column - code assumes tens can only be 10-20 and not anything higher
+
+            
+
+
+            StringBuilder ouput = new StringBuilder();
+            string[] ones_teens = {null, "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty", };
+            string[] tens = { "Ten", "Twenty", "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "One Hundred" };
+
+
+            //string numOneToTwenty = " " + ones_teens[(num % 100)];
+            string numOneToTwenty = (num % 100 > 0 && num > 99) ? " and " + ones_teens[(num % 100)] : ones_teens[(num % 100)];
+
+
+            num /= 100;
+
+
+            string numHundreds = ones_teens[num % 10] + " Hundred";
+
+
+            ouput.Append(numHundreds);
+            ouput.Append(numOneToTwenty);
+
+
+            Console.WriteLine(Convert.ToString(ouput));
+
+
+
+
+
+
+
+            //Console.WriteLine(num % 10);
+            //Console.WriteLine(ones[num % 10 - 1]);
+            //ouput.Insert(0, ones[num % 10 - 1]);
+
+            //num /= 10;
+            //Console.WriteLine(num);
+        }
+
+
+
         public static void happyNumber(int num)
         {
             List<int> list = new List<int>();
